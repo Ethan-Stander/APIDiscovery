c7ebefd8ee75278476c94fb1a19f356c0412e341
OofSpooder <ethanstander55@gmail.com>
2023-08-09 18:28:30 +0200

    displaying the stuff on the text message

--- app/src/main/java/com/example/prjapimk1/MainActivity.kt
+++ app/src/main/java/com/example/prjapimk1/MainActivity.kt
@@ -9,35 +9,34 @@ import com.google.gson.Gson
 import java.net.URL
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val show: TextView = findViewById(R.id.show) // Assuming 'show' is the ID of your TextView in XML layout
 
         val executor = Executors.newSingleThreadExecutor()
         executor.execute{
 
-            val url = URL("https://wordapidata.000webhostapp.com/?getuserdb")
+            val url = URL("https://wordapidata.000webhostapp.com/?userdb")
             val json = url.readText()
 
-                val userList = Gson().fromJson(json,Array<User>::class.java).toList();
+            val userList = Gson().fromJson(json, Array<User>::class.java).toList()
 
-            Handler(Looper.getMainLooper()).post{
-                Log.d("AddNewUser", "Plan Json vars" + json.toString())
-                Log.d("AddNewUser", "converted from")
+            val stringBuilder = StringBuilder()
 
+            for (user in userList) {
+                stringBuilder.append("Name: ${user.Name}\n")
+                stringBuilder.append("Password: ${user.Password}\n")
+                stringBuilder.append("Image URL: ${user.imageURL}\n\n")
             }
 
-
-
-
+            Handler(Looper.getMainLooper()).post{
+                show.text = stringBuilder.toString()
+            }
         }
-
-
-
-
-
     }
 }
